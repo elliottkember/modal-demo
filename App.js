80357fc2a0c6e32f773a61a5bd32a3252324bd54
Elliott Kember <elliott.kember@gmail.com>
2023-10-09 18:46:27 -0700

    wow

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+} from '@gorhom/bottom-sheet';
+import 'react-native-gesture-handler';
+import { ReactNativeZoomableView } from '@openspacelabs/react-native-zoomable-view';
+
+import {
   NavigationContainer,
   StackActions,
   useNavigation,
 } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React, { useEffect, useRef } from 'react';
+import React, {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from 'react';
 import {
   PanResponder,
   StyleSheet,
@@ -12,31 +25,37 @@ import {
   TouchableOpacity,
   View,
   Animated,
+  Button,
 } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Gesture, GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }} testID="app">
-      <View style={styles.container}>
-        <View style={{ width: '100%', height: '100%' }}>
-          <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
-              <Stack.Screen name="Home" component={HomeScreen} />
-              <Stack.Screen
-                name="Modal"
-                component={ModalStack}
-                options={{
-                  presentation: 'modal',
-                }}
-              />
-            </Stack.Navigator>
-          </NavigationContainer>
-        </View>
-      </View>
-    </GestureHandlerRootView>
+    <SafeAreaProvider>
+      <GestureHandlerRootView style={{ flex: 1 }} testID="app">
+        <BottomSheetModalProvider>
+          <View style={styles.container}>
+            <View style={{ width: '100%', height: '100%' }}>
+              <NavigationContainer>
+                <Stack.Navigator initialRouteName="Home">
+                  <Stack.Screen name="Home" component={HomeScreen} />
+                  <Stack.Screen
+                    name="Modal"
+                    component={ModalStack}
+                    options={{
+                      presentation: 'modal',
+                    }}
+                  />
+                </Stack.Navigator>
+              </NavigationContainer>
+            </View>
+          </View>
+        </BottomSheetModalProvider>
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   );
 }
 
@@ -67,8 +86,15 @@ const HomeScreen = () => {
 const Stack2 = createNativeStackNavigator();
 
 const ModalStack = () => (
-  <Stack2.Navigator initialRouteName="Modal">
-    <Stack2.Screen name="Modal" component={ModalView} />
+  <Stack2.Navigator initialRouteName="innermodal">
+    <Stack2.Screen
+      name="innermodal"
+      component={ModalView}
+      options={{
+        headerShown: false,
+        presentation: 'formSheet',
+      }}
+    />
   </Stack2.Navigator>
 );
 
@@ -81,48 +107,59 @@ const ModalView = () => {
     onStartShouldSetPanResponder: () => true,
     onPanResponderGrant: () => false,
     onMoveShouldSetPanResponder: () => true,
-    onPanResponderMove: Animated.event([null, { dx: pan.x, dy: pan.y }]),
-    onPanResponderRelease: () => {
-      pan.extractOffset();
+    onPanResponderMove: (evt, gestureState) => {
+      Animated.event([null, { dx: pan.x, dy: pan.y }], {})(evt, gestureState);
     },
   });
+
   const gestures2 = PanResponder.create({
     onShouldBlockNativeResponder: () => true,
     onStartShouldSetPanResponder: () => true,
     onPanResponderGrant: () => true,
     onMoveShouldSetPanResponder: () => true,
-    onPanResponderMove: Animated.event([null, { dx: pan2.x, dy: pan2.y }]),
-    onPanResponderRelease: () => {
-      pan.extractOffset();
+    onPanResponderMove: (evt, gestureState) => {
+      Animated.event([null, { dx: pan2.x, dy: pan2.y }], {})(evt, gestureState);
     },
   });
+
+  const bottomSheetModalRef = useRef(null);
+
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <View style={{ flexGrow: 1 }}>
-      <View
-        style={{
-          width: '100%',
-          height: '50%',
-          backgroundColor: '#ff9999',
+    <BottomSheetModalProvider>
+      <Button
+        title="Show bottom-sheet (fixes panning)"
+        onPress={() => {
+          bottomSheetModalRef.current?.present();
         }}
-        {...gestures.panHandlers}
+      />
+      <ReactNativeZoomableView
+        style={{ height: '33%' }}
+        maxZoom={100}
+        minZoom={1}
+        onShouldBlockNativeResponder={() => {
+          return true;
+        }}
+        initialZoom={1}
+        zoomStep={1}
+        contentWidth={800}
+        contentHeight={800}
+        panBoundaryPadding={50}
+        bindToBorders
+        movementSensibility={1}
+        pinchToZoomInSensitivity={1}
+        pinchToZoomOutSensitivity={1}
+        disableMomentum
       >
-        <Animated.View
-          style={{
-            backgroundColor: 'red',
-            padding: 24,
-            transform: [{ translateX: pan.x }, { translateY: pan.y }],
-            position: 'absolute',
-          }}
-        >
-          <Text>
-            I can't be moved around (onShouldBlockNativeResponder: false)
-          </Text>
-        </Animated.View>
-      </View>
+        <View style={{ backgroundColor: 'purple' }}>
+          <Text>hi</Text>
+        </View>
+      </ReactNativeZoomableView>
       <View
         style={{
           width: '100%',
-          height: '50%',
+          height: '33%',
           backgroundColor: '#99ff99',
         }}
         {...gestures2.panHandlers}
@@ -140,7 +177,36 @@ const ModalView = () => {
           </Text>
         </Animated.View>
       </View>
-    </View>
+      <View
+        style={{
+          width: '100%',
+          height: '33%',
+          backgroundColor: '#ff9999',
+        }}
+        {...gestures.panHandlers}
+      >
+        <Animated.View
+          style={{
+            backgroundColor: 'red',
+            padding: 24,
+            transform: [{ translateX: pan.x }, { translateY: pan.y }],
+            position: 'absolute',
+          }}
+        >
+          <Text>
+            I can't be moved around (onShouldBlockNativeResponder: false)
+          </Text>
+        </Animated.View>
+      </View>
+      <BottomSheetModal
+        ref={bottomSheetModalRef}
+        stackBehavior="push"
+        keyboardBlurBehavior="restore"
+        snapPoints={['50%', '95%']}
+      >
+        <Text>hi</Text>
+      </BottomSheetModal>
+    </BottomSheetModalProvider>
   );
 };
 
